f0ed90596dbe55f5da8b52aa1788d50288bdfbcb
Valentin Galfre <galfre.vn@gmail.com>
2023-01-26 23:54:34 -0300

    Enabling single quote formating in Prettier configuration

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,7 @@
 /** @type {import("prettier").Config} */
 module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  jsxSingleQuote: true,
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
